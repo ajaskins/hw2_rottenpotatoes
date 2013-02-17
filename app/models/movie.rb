27a327f646542693224d0231a8e326bf073aee81
saasbook <saasbook@saasbook.(none)>
2013-02-17 11:57:40 -0800

    cleaning still

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
-    def all_ratings
+    def self.all_ratings
         array = Array.new
-        select("rating").uniq.each {|mov| array.push(mov.rating)}
+        self.select("rating").uniq.each {|mov| array.push(mov.rating)}
         array.sort.uniq
     end
 end
