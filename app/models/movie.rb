ecb8155921d768858e52ba468eb2ef338426b952
saasbook <saasbook@saasbook.(none)>
2013-02-17 11:54:47 -0800

    clean up hwk part 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
-    def self.all_ratings
-        a = Array.new
-        self.select("rating").uniq.each {|el| a.push(el.rating)}
-        a.sort.uniq
+    def all_ratings
+        array = Array.new
+        select("rating").uniq.each {|mov| array.push(mov.rating)}
+        array.sort.uniq
     end
 end
